749202220d028e3af721f5c069c2368a61886f08
Nikulair <niekhoevenaars@gmail.com>
2025-12-15 14:27:43 +0100

    readded the migrations

---
+++ apps/server/server/Migrations/20251215132632_AddPrivateAppointmentAndSpecialistIcalTables.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZhmApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrivateAppointmentAndSpecialistIcalTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SpecialistIcals",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastCalendarHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastSyncAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialistIcals", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_SpecialistIcals_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SpecialistPrivateAgendas",
+                columns: table => new
+                {
+                    Uid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpecialistPrivateAgendas", x => x.Uid);
+                    table.ForeignKey(
+                        name: "FK_SpecialistPrivateAgendas_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "28479351-1d36-400f-a7e0-8fb9a1dc7b11");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "9dca51dc-50bc-4b71-bdab-b69ea4c49e67");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "2dea6453-afe2-4a95-9ee8-62312cf68abe");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "b16305ba-7162-43bb-9678-96c309bc9ec0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "666e791e-1450-4e4f-bf65-2292ba4c4f37");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "fecbe247-8fad-4735-a473-26980b644835");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpecialistPrivateAgendas_UserId",
+                table: "SpecialistPrivateAgendas",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpecialistIcals");
+
+            migrationBuilder.DropTable(
+                name: "SpecialistPrivateAgendas");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "2078fd50-2034-4e6e-83b3-c697e4e6d467");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "fd1f5e6f-f69c-4d36-9290-a7294658f078");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "59c356ed-e187-4803-b1b6-b2518d6deb6f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "1fd4d8fd-a84e-42d3-8aa6-a2119119155d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ConcurrencyStamp",
+                value: "08bff71d-9660-4931-8e93-25d890bdbd8d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ConcurrencyStamp",
+                value: "894d29c3-8b65-459e-9a8a-1cdc562e00c1");
+        }
+    }
+}
